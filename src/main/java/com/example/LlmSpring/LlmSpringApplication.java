9e16b75e1d94880a343870f320d7bbed2262e9ae
sgn08062 <sgn08062@gmail.com>
2026-01-20 15:02:50 +0900

    Init Commit

---
+++ src/main/java/com/example/LlmSpring/LlmSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.LlmSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmSpringApplication.class, args);
+	}
+
+}
